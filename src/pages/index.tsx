da1f809402320121aaf670239d358a810fe57197
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2023-07-31 19:43:16 +0400

    lorem ipsum on main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,18 @@ import * as React from "react";
 
 import type { HeadFC, PageProps } from "gatsby";
 
-import ServerRenderableComponent from "../components/static-component";
-import DemoGrid from "../components/demo-grid";
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <ServerRenderableComponent/>
-      <DemoGrid/>
+      <h1>Main Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
     </main>
   )
 }
@@ -19,7 +23,6 @@ export default IndexPage
 export const Head: HeadFC = () => {
   return (
     <>
-      <link rel="stylesheet" type="text/css" href="https://cdn3.devexpress.com/jslib/23.1.3/css/dx.light.css" />
       <title>Home Page</title>
     </>
   )
